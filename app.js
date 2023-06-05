b8f068783b6cb486196358da458ae48b6e90f8fa
olahkrisz1 <olahkrisz1@gmail.com>
2023-06-05 13:50:40 +0200

    lesson 1,2,3

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI = "mongodb+srv://cerbera:cerbera@cluster0.pglmjwk.mongodb.net/";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3200))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
